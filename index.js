c955837ad31b72c25d7972017c06635da4d3b8e0
bmilo327 <bernathmilan7924@gmail.com>
2026-03-18 20:43:37 +0100

    tesztesetek keszen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { SAKKLISTA } from "./adat.js";
 import { rendezes, poziciok, kiiras } from "./fuggvenyek.js";
 import { sorokMegjelenit } from "./tabla.js";
+import { utesEllenorzesTeszt } from "./teszt/utesVizsgalatTeszt.js";
 rendezes(SAKKLISTA)
 kiiras(poziciok());
 sorokMegjelenit(SAKKLISTA);
--- teszt/utesVizsgalatTeszt.js
+++ teszt/utesVizsgalatTeszt.js
@@ -1,5 +1,7 @@
 import { utesEllenorzes } from "../fuggvenyek.js";
 
+
+
 const esetLista = [
     {
         pozicio1 : "a1",
@@ -23,16 +25,40 @@ const esetLista = [
         pozicio1 : "a1",
         pozicio2 : "h8",
         vart : true,
-        szoveg : "egy mezőn nem lehet két bábu"
+        szoveg : "bábuk ütik egymást sorban"
     },
     {
         pozicio1 : "a1",
-        pozicio2 : "a1",
+        pozicio2 : "a8",
+        vart : true,
+        szoveg : "bábuk ütik egymást oszlopban"
+    },
+    {
+        pozicio1 : "a1",
+        pozicio2 : "d8",
         vart : false,
-        szoveg : "egy mezőn nem lehet két bábu"
+        szoveg : "bábuk nem ütik egymást"
+    },
+    {
+        pozicio1 : "",
+        pozicio2 : "",
+        vart : false,
+        szoveg : "nincs pozíció megadva"
+    },
+    {
+        pozicio1 : "a12",
+        pozicio2 : "xc34",
+        vart : false,
+        szoveg : "nincsenek ilyen mezők a táblán"
     },
 ]
 
-function utesVizsgalatTeszt() {
-     
+export function utesEllenorzesTeszt() {
+    for (let index = 0; index < esetLista.length; index++) {
+        let eredmeny = utesEllenorzes(esetLista[index].pozicio1, esetLista[index].pozicio2);
+        console.assert(eredmeny === esetLista[index].vart, `pozicio1=${esetLista[index].pozicio1}, pozicio2=${esetLista[index].pozicio2}, elvárt:${esetLista[index].vart}, kapott:${eredmeny}`, esetLista[index].szoveg);
+    }
+    console.log("Minden teszt lefutott");
 }
+
+utesEllenorzesTeszt();
